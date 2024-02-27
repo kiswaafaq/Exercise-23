f95f4c2a8423a2da89a209e9158d7212dac10ab7
kiswaafaq <kiswaafaque14@gmail.com>
2024-02-26 23:31:53 -0800

    First commit

---
+++ conditional_tests.js
@@ -0,0 +1,12 @@
+"use strict";
+// car variable
+let car = 'sabaru';
+//Test No.1
+console.log("Is car === 'sabaru'? I predict True");
+console.log(car === 'sabaru');
+//Test No.2
+console.log("Is car == 'sabaru'? I predict True");
+console.log(car == 'sabaru');
+//Test No.3
+console.log("Is car != 'Honda'? I predict True");
+console.log(car != 'Honda');
---
+++ conditional_tests.ts
@@ -0,0 +1,44 @@
+
+// car variable
+let car : string = 'sabaru';
+
+
+//Test No.1
+console.log("Is car === 'sabaru'? I predict True");
+console.log(car ==='sabaru');
+
+//Test No.2
+console.log("Is car == 'sabaru'? I predict True");
+console.log(car =='sabaru');
+
+//Test No.3
+console.log("Is car != 'Honda'? I predict True");
+console.log(car !='Honda');
+
+//Test No.4
+console.log("Is car !== 'Ford'? I predict True");
+console.log(car !=='Ford');
+
+//Test No.5
+console.log("Is car.Upper case == 'SABARU'? I predict True");
+console.log(car.toUpperCase() =='SABARU');
+
+//Test No.6
+console.log("Is car == 'SABARU'? I predict False");
+console.log(car =='SABARU');
+
+//Test No.7
+console.log("Is car === 'SABARU'? I predict False");
+console.log(car === 'SABARU');
+
+//Test No.8
+console.log("Is car === 'Train'? I predict False");
+console.log(car === 'Train');
+
+//Test No.9
+console.log("Is car === 'Bus'? I predict False");
+console.log(car === 'Bus');
+
+//Test No.10
+console.log("Is car === 'Cycle'? I predict False");
+console.log(car === 'Cycle');
